972215501e2c997e2b5e32681fce59b36bb13382
bestplanetarian <nagisa6557@163.com>
2018-02-16 21:57:26 -0600

    Summary

---
+++ src/taskers/Task1.java
@@ -0,0 +1,59 @@
+
+package taskers;
+
+import javafx.application.Platform;
+
+/**
+ *
+ * @author dalemusser
+ * 
+ * This example uses an object passed in with a notify()
+ * method that gets called when a notification is to occur.
+ * To accomplish this the Notifiable interface is needed.
+ * 
+ */
+public class Task1 extends Thread {
+    
+    private int maxValue, notifyEvery;
+    boolean exit = false;
+    
+    private Notifiable notificationTarget;
+    
+    public Task1(int maxValue, int notifyEvery)  {
+        this.maxValue = maxValue;
+        this.notifyEvery = notifyEvery;
+    }
+    
+    @Override
+    public void run() {
+        doNotify("Task1 start.");
+        for (int i = 0; i < maxValue; i++) {
+            
+            if (i % notifyEvery == 0) {
+                doNotify("It happened in Task1: " + i);
+            }
+            
+            if (exit) {
+                return;
+            }
+        }
+        doNotify("Task1 done.");
+    }
+    
+    public void end() {
+        exit = true;
+}
+    
+    public void setNotificationTarget(Notifiable notificationTarget) {
+        this.notificationTarget = notificationTarget;
+    }
+    
+    private void doNotify(String message) {
+        // this provides the notification through a method on a passed in object (notificationTarget)
+         if (notificationTarget != null) {
+            Platform.runLater(() -> {
+                notificationTarget.notify(message);
+            });
+        }
+    }
+}
